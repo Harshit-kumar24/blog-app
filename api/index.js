3ed33e01c3e3e604f6a70e8dbc41497a0cd90c24
Harshit Kumar <harshitkumar9548@gmail.com>
2023-02-07 02:32:38 +0530

    commit

--- api/index.js
+++ api/index.js
@@ -7,10 +7,11 @@ const userRoute = require("./routes/users");
 const postRoute = require("./routes/posts");
 const categoryRoute = require("./routes/categories");
 const {PORT, MONGO_URL} = require('./config/serverConfig');
-
+const  path  = require('path');
 
 //to send the data in json format
 app.use(express.json());
+app.use("/images",express.static(path.join(__dirname,"/images")));
 
 //to supress deprication warning
 mongoose.set('strictQuery',false);
